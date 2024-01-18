e914c1b57bc88a126d45df72f44f3a0393553fd9
notauhuynh <142616016+notauhuynh@users.noreply.github.com>
2024-01-17 19:46:29 -0800

    Update PalindromeTester.java

--- src/test/java/PalindromeTester.java
+++ src/test/java/PalindromeTester.java
@@ -25,7 +25,6 @@ public class PalindromeTester {
 
    @Test
    public void word(){
-        PalindromeChecker sim = new PalindromeChecker();
         assertEquals(false, sim.palindrome("test"));
         assertEquals(true, sim.palindrome("rotator"));
         assertEquals(false, sim.palindrome("rewriter"));
